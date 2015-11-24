21c5db6653aecf63c7ea4453522a97ab898d2ddd
Dhens <rudenyl.eeweb@gmail.com>
2015-11-24 16:10:22 +0800

    Add new markdown stubs for admin/frontend; Add frontend event listeners; Add missing markdown dependency files.

--- Hooks/ArticlesAdminOnBeforeRenderItem.php
+++ Hooks/ArticlesAdminOnBeforeRenderItem.php
@@ -7,5 +7,5 @@ class ArticlesAdminOnBeforeRenderItem
 {
     public function handle($article)
     {
-    }   
+    }
 }
--- Hooks/ArticlesOnAfterRenderItem.php
+++ Hooks/ArticlesOnAfterRenderItem.php
@@ -3,11 +3,11 @@ namespace Plugins\MarkDown\Hooks;
 
 use OroCMS\Admin\Stub;
 
-class ArticlesAdminOnAfterRenderItem
+class ArticlesOnAfterRenderItem
 {
     public function handle($view)
     {
-        $buffer = (new Stub(__DIR__ . '/../Stubs/admin.markdown.stub', []))->render();
+        $buffer = (new Stub(__DIR__ . '/../Stubs/markdown.stub', []))->render();
         $view->getFactory()->startSection('header', $buffer);
     }   
 }
